206d8c6a573fe4637968dd62de15c66051d1de33
anjagfrerer <anja.gfrerer01@gmail.com>
2026-03-16 08:32:22 +0100

    Login-Page html and css, not included yet, just seperate

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,14 @@ import { RouterOutlet, RouterModule } from '@angular/router';
 import { SidebarComponent } from "../components/sidebar/sidebar";
 import { ToursPageComponent } from "../components/toursPage/toursPage";
 import { HeaderComponent } from "../components/header/header";
+import { LoginPageComponent } from '../components/loginPage/loginPage';
 
 /**
  * Root: Komponente, mit der Angular die ganze App startet
  */
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, SidebarComponent, ToursPageComponent, HeaderComponent ],
+  imports: [RouterOutlet, RouterModule, SidebarComponent, LoginPageComponent, ToursPageComponent, HeaderComponent ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/components/loginPage/loginPage.ts
@@ -0,0 +1,11 @@
+import { Component, computed, signal } from "@angular/core";
+
+// ViewModel
+@Component({
+    selector: 'loginPage',
+    standalone: true,
+    templateUrl: './loginPage.html'
+})
+
+export class LoginPageComponent {
+}
