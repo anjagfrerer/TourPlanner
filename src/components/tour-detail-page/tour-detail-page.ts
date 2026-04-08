838ea185550b8262d6c64371ca117c0cf383402a
munsinator <munseey@icloud.com>
2026-04-08 21:34:39 +0200

    comment section wip

--- src/components/tour-detail-page/tour-detail-page.ts
+++ src/components/tour-detail-page/tour-detail-page.ts
@@ -1,14 +1,17 @@
 import { Component, computed, signal } from "@angular/core";
 import { StarRating } from "../star-rating/star-rating";
 import { BackButton } from "../back-button/back-button";
+import { TourLogsItemComponent } from "../TourLog/tour-log-item/tour-log-item"
+import { TourLog } from "../../app/models/tour-log.model";
 
 // ViewModel
 @Component({
     selector: 'tour-detail',
     templateUrl: './tour-detail-page.html',
-    imports: [StarRating, BackButton],
+    imports: [StarRating, BackButton, TourLogsItemComponent],
 })
 
 export class TourDetailComponent {
+    tourlogs = signal<TourLog[]>([]);
 
 }
