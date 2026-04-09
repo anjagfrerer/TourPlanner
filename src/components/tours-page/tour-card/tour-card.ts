55c28cfe516d8bdeb63bf78dbf6b699cf99cbded
munsinator <munseey@icloud.com>
2026-04-09 15:43:31 +0200

    tour card wip

--- src/components/tours-page/tour-card/tour-card.ts
+++ src/components/tours-page/tour-card/tour-card.ts
@@ -2,13 +2,16 @@ import { Component, inject, input, effect } from "@angular/core";
 import { StarRating } from "../../star-rating/star-rating";
 import { Tour } from "../../../app/models/tour.model";
 import { RouterLink } from "@angular/router";
+import { TourCardViewModel } from "./tour-card.vm";
 
 @Component({
     selector: 'tour-card',
     templateUrl: './tour-card.html',
     imports:[StarRating, RouterLink],
+    providers: [TourCardViewModel]
 })
 
 export class TourCard {
+    vm = inject(TourCardViewModel);
     tour = input.required<Tour>();
 }
