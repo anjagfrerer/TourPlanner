2b95ee984a901109dc01c743c24ffd93a7cd78b5
Eunice M. <munseey@icloud.com>
2026-06-03 11:52:59 +0200

    clean up

--- backend/src/main/java/at/technikum/backend/config/SecurityConfig.java
+++ backend/src/main/java/at/technikum/backend/config/SecurityConfig.java
@@ -74,18 +74,4 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
 }
