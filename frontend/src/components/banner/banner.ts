dc81f7217e293d28d7799819f4045be557475e4e
anjagfrerer <anja.gfrerer01@gmail.com>
2026-06-22 15:57:35 +0200

    fix(frontend): update banner component

--- frontend/src/components/banner/banner.ts
+++ frontend/src/components/banner/banner.ts
@@ -6,12 +6,13 @@ import { Router } from "@angular/router";
 @Component({
   selector: 'banner',
   templateUrl: './banner.html',
+  styleUrls: ['./banner.css'],
   imports: [SearchInput]
 })
 export class BannerComponent {
   private searchService = inject(SearchService);
   public router = inject(Router);
-
+  
   handleSearch(text: string): void {
     this.searchService.searchTerm.set(text);
   }
